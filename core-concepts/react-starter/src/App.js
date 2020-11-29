7b3841a67ffe29e7564d332ff43f4fbc4e106323
Adam Hauze <hauze_v@protonmail.com>
2020-11-29 13:00:40 -0500

    Learned about useEffect and some different scenarios in which it might be useful.

--- core-concepts/react-starter/src/App.js
+++ core-concepts/react-starter/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CreateTweet from "./components/CreateTweet";
 import TweetList from "./components/TweetList";
 import "./styles/App.css";
@@ -8,6 +8,12 @@ function App() {
   const [name, setName] = useState("Adam Savage");
   const [textInput, setTextInput] = useState("");
   const [tweets, setTweets] = useState([]);
+
+  /* The second argument we pass to useEffect determines which component update the function should run after
+     Note that it will also run immediately when the main component (file) is mounted/run */
+  useEffect(() => {
+    console.log("we run a function, yay!");
+  }, [tweets]);
 
   return (
     <div className="App">
