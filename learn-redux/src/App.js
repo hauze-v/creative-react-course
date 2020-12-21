66054bb44f9e8d041ac934c42a52ec0ad16c6593
Adam Hauze <hauze_v@protonmail.com>
2020-12-21 16:44:49 -0500

    Walked through a simple Redux example.

---
+++ learn-redux/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter: 0</h1>
+      <button>Increment</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ learn-redux/src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+
+// ACTION
+const increment = () => {
+  return {
+    name: "INCREMENT",
+  };
+};
+
+// REDUCER first argument is the initial state and the second is the action
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+// Store is like GLOBALIZED STATE
+let store = createStore(counterReducer);
+
+// DISPATCH
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
